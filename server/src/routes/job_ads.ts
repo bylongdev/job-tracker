62b60c9e69c9885c5d2aab61ef9233f6580a8b57
NightW0lfff <philong170101@gmail.com>
2025-12-27 11:24:52 +1100

    feat: Add Update api for job_ads.

--- server/src/routes/job_ads.ts
+++ server/src/routes/job_ads.ts
@@ -90,6 +90,64 @@ router.get("/:id", async (req: Request, res: Response) => {
 
 // UPDATE:
 
-router.patch("/:id");
+router.patch("/:id", async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params;
+		const allowedList = new Set([
+			"recruiter_id",
+			"company_name",
+			"job_title",
+			"job_description",
+			"published_at",
+			"location",
+			"job_type",
+			"platform",
+			"url",
+			"skill_requirements",
+			"tech_stack",
+			"expired_at",
+			"salary_range",
+		]);
+
+		// Filter out key not valid and empty value
+		const entries = Object.entries(req.body).filter(
+			([key, value]) => allowedList.has(key) && value != undefined
+		);
+
+		// Return if not valid fields
+		if (entries.length === 0) {
+			return res.status(400).json({ error: "No valid fields to update" });
+		}
+
+		const sets: string[] = []; // store the field
+		const params: any[] = []; // store the value
+
+		// loop through the entries to push the key and value into 2 array to perform sql injection
+		for (const [key, value] of entries) {
+			params.push(value);
+			sets.push(`${key} = $${params.length}`);
+		}
+
+		// updated field updated_at to current timestamp
+		sets.push("updated_at = NOW()");
+
+		params.push(id);
+
+		const sql = `UPDATE job_ads SET ${sets.join(", ")} WHERE id = $${
+			params.length
+		} RETURNING *`;
+
+		const result = await pool.query(sql, params);
+
+		if (result.rowCount === 0)
+			return res.status(404).json({ error: "Not found" });
+
+		return res.json(result.rows[0]);
+	} catch (e: any) {
+		if (e.code === "23505")
+			return res.status(409).json({ error: "URL already exists" });
+		res.status(500).json({ error: e.message });
+	}
+});
 
 export default router;
