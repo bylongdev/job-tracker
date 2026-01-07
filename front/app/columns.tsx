7fe7f93037020e6a071c9738bd291fc93933c853
NightW0lfff <philong170101@gmail.com>
2026-01-08 04:19:32 +1100

    feat: Add basic table for job_ads section.

---
+++ front/app/columns.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+/*   company_name: string;
+  job_title: string;
+  job_description: string;
+  published_at: Date;
+  location: string;
+  job_type:
+    | ["Full-time", "Part-time", "Casual", "Contract", "Internship"]
+    | string;
+  source: string;
+  url: string;
+  skill_requirements?: string;
+  tech_stack?: string;
+  expired_at?: Date;
+  salary_min?: string;
+  salary_max?: string;
+  created_at: Date; */
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type JobAds = {
+  company_name: string;
+  job_title: string;
+  published_at: Date;
+  location: string;
+  job_type:
+    | ["Full-time", "Part-time", "Casual", "Contract", "Internship"]
+    | string;
+  salary_min?: string;
+  salary_max?: string;
+  created_at: Date;
+};
+
+export const columns: ColumnDef<JobAds>[] = [
+  {
+    accessorKey: "company_name",
+    header: () => <div className="text-right">Company Name</div>,
+  },
+  {
+    accessorKey: "job_title",
+    header: "Job Title",
+  },
+  {
+    accessorKey: "published_at",
+    header: "Published At",
+  },
+  {
+    accessorKey: "location",
+    header: "Location",
+  },
+  {
+    accessorKey: "job_type",
+    header: "Job Type",
+  },
+  {
+    accessorKey: "salary_min",
+    header: "Salary Min",
+  },
+  {
+    accessorKey: "salary_max",
+    header: "Salary Max",
+  },
+  {
+    accessorKey: "created_at",
+    header: "Created At",
+  },
+];
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,5 +1,41 @@
-import JobTable from "./components/jobTable";
+import { columns } from "./columns";
 import NavBar from "./components/navBar";
+import { DataTable } from "./data-table";
+
+type JobAds = {
+  company_name: string;
+  job_title: string;
+  published_at: Date;
+  location: string;
+  job_type:
+    | ["Full-time", "Part-time", "Casual", "Contract", "Internship"]
+    | string;
+  salary_min?: string;
+  salary_max?: string;
+  created_at: Date;
+};
+
+const jobAds: JobAds[] = [
+  {
+    company_name: "A",
+    job_title: "A",
+    published_at: new Date(),
+    location: "A",
+    job_type: "Casual",
+    salary_min: "100",
+    salary_max: "200",
+    created_at: new Date(),
+  },
+  {
+    company_name: "B",
+    job_title: "B",
+    published_at: new Date(),
+    location: "B",
+    job_type: "C",
+    salary_min: "100",
+    created_at: new Date(),
+  },
+];
 
 export default function Home() {
   return (
@@ -9,7 +45,7 @@ export default function Home() {
 
       {/* Content Section */}
       <main className="grow">
-        <JobTable />
+        <DataTable columns={columns} data={jobAds} />
       </main>
     </div>
   );
